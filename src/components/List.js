83679542e4eeaaa6950bbc13fdd659192df8c9eb
Seline van Dijke <seline.vandijke98@gmail.com>
2022-10-26 18:44:31 +0200

    Opbouw React Shopping List

---
+++ src/components/List.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '/Users/selinevandijke/Documents/Shopping-List/my-shopping-list/src/App.js'
+
+class List extends React.Component {
+
+    render() {
+        return (
+            < div className = "grocery-list" >
+                <ul>
+                    <li>Bread</li>
+                    <li>Cucumber</li>
+                    <li>Coffee</li>
+                </ul> 
+            </div >
+        )
+    }
+}
+
+export default List
