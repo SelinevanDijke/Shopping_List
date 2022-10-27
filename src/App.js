2fe660881f82d081816dbaf1dfea00064142b376
Seline van Dijke <seline.vandijke98@gmail.com>
2022-10-27 21:16:21 +0200

    Part 1 - Shopping list

--- src/App.js
+++ src/App.js
@@ -3,33 +3,16 @@ import './App.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './components/List.js';
+import List from './components/List.js';
+import { render } from '@testing-library/react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<List />);
 
 function App() {
-
-  
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  
+    
+    return (
+        <List />
+    )
+    
 }
 
-
-
-export default App;
+export default App
--- src/components/List.js
+++ src/components/List.js
@@ -1,18 +1,17 @@
 import React from 'react'
-import '/Users/selinevandijke/Documents/Shopping-List/my-shopping-list/src/App.js'
+import App from '/Users/selinevandijke/Documents/Shopping-List/my-shopping-list/src/App.js';
 
 class List extends React.Component {
 
     render() {
         return (
-            < div className = "grocery-list" >
+            <div className="grocery-list">
                 <ul>
                     <li>Bread</li>
                     <li>Cucumber</li>
                     <li>Coffee</li>
                 </ul> 
-            </div >
-            //test
+            </div>
         )
     }
 }
